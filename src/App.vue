f42d6cfd70564159ba485f06ac5b01b6bcabb367
lixiangparadise <18862107076@163.com>
2020-05-08 22:12:20 +0800

    first init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <span class="icon-back"></span>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+document.addEventListener('DOMContentLoaded', () => {
+  //将字体大小设置为根元素的十分之一
+  const html = document.querySelector('html');
+  let fontSize = window.innerWidth / 10; 
+  fontSize = fontSize > 50 ? 50 : fontSize;
+  html.style.fontSize = fontSize + 'px';
+});
+</script>
+
+<style>
+
+</style>
